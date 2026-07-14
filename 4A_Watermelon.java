9258ec249bd61a24e9bd43e796bfa16f75208e66
parthnehe2006 <parthnehe2006@gmail.com>
2026-07-14 21:21:14 +0530

    Day_1

---
+++ 4A_Watermelon.java
@@ -0,0 +1,25 @@
+﻿import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double split = sc.nextDouble();
+        if (split%2==00 && split>2){
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+
+        sc.close();
+    }
+}
+
+
+
+
+
+
+
+ 
+    
