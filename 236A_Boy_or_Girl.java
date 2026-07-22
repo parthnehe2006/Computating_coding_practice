4b6a126c13a679bee215fce190ac4ef77b922c6d
parthnehe2006 <parthnehe2006@gmail.com>
2026-07-22 22:21:38 +0530

    Day_9
    Day_9

---
+++ 236A_Boy_or_Girl.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            boolean found = false;
+
+            // Check if current character appeared before
+            for (int j = 0; j < i; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                count++;
+            }
+        }
+
+        if (count % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        } else {
+            System.out.println("IGNORE HIM!");
+        }
+
+        sc.close();
+    }
+}
