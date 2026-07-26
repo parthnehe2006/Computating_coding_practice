49b1c1eb99704c59e05791f143550fef138f256a
parthnehe2006 <parthnehe2006@gmail.com>
2026-07-26 23:36:49 +0530

    Day_13
    Day_13

---
+++ 96A_football.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count >= 7) {
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        System.out.println("NO");
+    }
+}
