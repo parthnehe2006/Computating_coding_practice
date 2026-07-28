98347d73287e5b5e833431a9ca825ca26e742d39
parthnehe2006 <parthnehe2006@gmail.com>
2026-07-28 21:29:06 +0530

    Day_14
    Day_14

---
+++ 281A_Word_Capitalization.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        s = Character.toUpperCase(s.charAt(0)) + s.substring(1);
+
+        System.out.println(s);
+
+        sc.close();
+    }
+}
