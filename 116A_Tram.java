06dee4b741135a5e6a1e213c26d32b63a04979d3
parthnehe2006 <parthnehe2006@gmail.com>
2026-08-08 23:49:12 +0530

    Day-24
    Day-24

---
+++ 116A_Tram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int current = 0;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt(); // people exiting
+            int b = sc.nextInt(); // people entering
+
+            current = current - a + b;
+
+            max = Math.max(max, current);
+        }
+
+        System.out.println(max);
+    }
+}
