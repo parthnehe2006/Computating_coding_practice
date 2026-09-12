e94b2c5cdd8bfa574916249e705ec0b63c0eee69
parthnehe2006 <parthnehe2006@gmail.com>
2026-09-12 22:02:29 +0530

    Day_50
    Day_50

---
+++ 228A_Is your horseshoe on the other hoof.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(a);
+        set.add(b);
+        set.add(c);
+        set.add(d);
+
+        System.out.println(4 - set.size());
+    }
+}
